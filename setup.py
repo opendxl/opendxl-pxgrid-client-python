c5623b48687032e14e48d5f3aef83d4f073c1263
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-03-16 10:13:21 -0700

    Update pylint glob to parse all sample files

--- setup.py
+++ setup.py
@@ -31,7 +31,12 @@ class LintCommand(Command):
     def run(self):
         self.announce("Running pylint for library source files and tests",
                       level=distutils.log.INFO)
-        subprocess.check_call(["pylint", "dxlciscopxgridclient"] + glob.glob("*.py"))
+        subprocess.check_call(["pylint", "dxlciscopxgridclient"] +
+                              glob.glob("*.py"))
+        self.announce("Running pylint for samples", level=distutils.log.INFO)
+        subprocess.check_call(["pylint"] + glob.glob("sample/*.py") +
+                              glob.glob("sample/**/*.py") +
+                              ["--rcfile", ".pylintrc.samples"])
 
 
 class CiCommand(Command):
