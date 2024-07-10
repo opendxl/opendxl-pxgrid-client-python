59a71456dd0b16e4b63056f170633e8c7a05f7fa
harrison-ahearn <harrison.ahearn@trellix.com>
2024-07-10 16:52:30 -0400

    client, callback upgrade to pxgrid2

--- dxlciscopxgridclient/callbacks.py
+++ dxlciscopxgridclient/callbacks.py
@@ -2,177 +2,57 @@ from __future__ import absolute_import
 from dxlbootstrap.util import MessageUtils
 from dxlclient.callbacks import EventCallback
 
-
-class AncApplyEndpointPolicyCallback(EventCallback):
+class AncStatusCallback(EventCallback):
     """
-    Callback wrapper which invokes the :meth:`on_apply_endpoint_policy` method
-    when an apply endpoint policy event is received.
+    Callback wrapper which is invoked on all ANC notifications
     """
     def on_event(self, event):
         """
-        Method invoked when an apply endpoint policy event occurs. Derived
-        classes can implement the :meth:`on_apply_endpoint_policy` method
-        instead of this method in order to process the event payload as a
-        `dict`.
+        Method invoked when a status notification event occurs. Derived classes can
+        implement the :meth:`on_session_notification` method instead of this method in order
+        to process the event payload as a `dict`.
 
         :param dxlclient.message.Event event: Message received for the event
             notification
         """
-        self.on_apply_endpoint_policy(MessageUtils.json_payload_to_dict(event))
+        self._on_status_notification(MessageUtils.json_payload_to_dict(event))
 
-    def on_apply_endpoint_policy(self, apply_dict):
+    def _on_status_notification(self, status_dict):
         """
-        Method invoked when an apply endpoint policy event occurs.
+        Method invoked when a status notification event occurs.
 
-        :param dict apply_dict: Payload for the event notification. For
-            example, the value for an event sent on application of an endpoint
-            policy should look similar to this:
+        :param dict status_dict: Payload for the event notification.
+            Example status_dict look similar to this:
 
-            .. code-block:: json
-
-                {
-                    "ipAddress": "192.168.1.1",
-                    "policyName": "quarantine_policy"
+            {
+                "command": "MESSAGE",
+                "content": "eyJvcGVyYXRpb25JZCI6ImNpc2UucHNhcmNobGFiLmNvbToxMDYiLCJtYWNBZGRyZXNzIjoiMDI6QjU6NDc6RTI6MEU6NzMiLCJzdGF0dXMiOiJTVUNDRVNTIiwicG9saWN5TmFtZSI6IkFOQ19TaHV0In0=",
+                "headers": {
+                    "content-length": "116",
+                    "destination": "/topic/com.cisco.ise.config.anc.status",
+                    "message-id": "106527",
+                    "subscription": "1"
                 }
+            }
+        
+            where the content is the base64 encoded content of the event. The content
+            after being decoded and json.loads()-ed will be similar to this format:
+
+            {
+                "macAddress": "00:11:22:33:44:55",
+                "operationId": "cise.psarchlab.com:104",
+                "policyName": "ANC_Shut",
+                "status": "SUCCESS"
+            }
+
+            Note, policyName is not required for all events and just a status and endpoint
+            identifier can be present.
+            See https://github.com/cisco-pxgrid/pxgrid-rest-ws/wiki/ANC-configuration#objects for more information
+
+        See the example _on_status_notification() handler in basic_anc_status_notification_example.py
+        for a simple handler that decodes and json.loads().
         """
         pass
-
-
-class AncClearEndpointPolicyCallback(EventCallback):
-    """
-    Callback wrapper which invokes the :meth:`on_clear_endpoint_policy` method
-    when a clear endpoint policy event is received.
-    """
-    def on_event(self, event):
-        """
-        Method invoked when a clear endpoint policy event occurs. Derived
-        classes can implement the :meth:`on_clear_endpoint_policy` method
-        instead of this method in order to process the event payload as a
-        `dict`.
-
-        :param dxlclient.message.Event event: Message received for the event
-            notification
-        """
-        self.on_clear_endpoint_policy(MessageUtils.json_payload_to_dict(event))
-
-    def on_clear_endpoint_policy(self, clear_dict):
-        """
-        Method invoked when a clear endpoint policy event occurs.
-
-        :param dict clear_dict: Payload for the event notification. For
-            example, the value for an event sent on clearing of an endpoint
-            policy should look similar to this:
-
-            .. code-block:: json
-
-                {
-                    "ipAddress": "192.168.1.1"
-                }
-        """
-        pass
-
-
-class AncCreatePolicyCallback(EventCallback):
-    """
-    Callback wrapper which invokes the :meth:`on_create_policy` method when a
-    clear policy event is received.
-    """
-    def on_event(self, event):
-        """
-        Method invoked when an create policy event occurs. Derived classes can
-        implement the :meth:`on_create_policy` method instead of this method in
-        order to process the event payload as a `dict`.
-
-        :param dxlclient.message.Event event: Message received for the event
-            notification
-        """
-        self.on_create_policy(MessageUtils.json_payload_to_dict(event))
-
-    def on_create_policy(self, create_dict):
-        """
-        Method invoked when a create policy event occurs.
-
-        :param dict create_dict: Payload for the event notification. For
-            example, the value for an event sent on creation of a policy should
-            look similar to this:
-
-            .. code-block:: json
-
-                {
-                    "action": [ "Quarantine" ],
-                    "name": "test_policy"
-                }
-        """
-        pass
-
-
-class AncUpdatePolicyCallback(EventCallback):
-    """
-    Callback wrapper which invokes the :meth:`on_update_policy` method
-    when an update policy event is received.
-    """
-    def on_event(self, event):
-        """
-        Method invoked when a update policy event occurs. Derived classes can
-        implement the :meth:`on_update_policy` method instead of this method in
-        order to process the event payload as a `dict`.
-
-        :param dxlclient.message.Event event: Message received for the event
-            notification
-        """
-        self.on_update_policy(MessageUtils.json_payload_to_dict(event))
-
-    def on_update_policy(self, update_dict):
-        """
-        Method invoked when an update policy event occurs.
-
-        :param dict update_dict: Payload for the event notification. For
-            example, the value for an event sent on update of a policy should
-            look similar to this:
-
-            .. code-block:: json
-
-                {
-                    "action": [ "Quarantine" ],
-                    "name": "test_policy"
-                }
-        """
-        pass
-
-
-class AncDeletePolicyCallback(EventCallback):
-    """
-    Callback wrapper which invokes the :meth:`on_delete_policy` method
-    when a delete policy event is received.
-    """
-    def on_event(self, event):
-        """
-        Method invoked when a delete policy event occurs. Derived classes can
-        implement the :meth:`on_delete_policy` method instead of this method in
-        order to process the event payload as a `dict`.
-
-        :param dxlclient.message.Event event: Message received for the event
-            notification
-        """
-        self.on_delete_policy(MessageUtils.json_payload_to_dict(event))
-
-    def on_delete_policy(self, delete_dict):
-        """
-        Method invoked when an delete policy event occurs.
-
-        :param dict delete_dict: Payload for the event notification. For
-            example, the value for an event sent on deletion of a policy should
-            look similar to this:
-
-            .. code-block:: json
-
-                {
-                    "action": [ "Quarantine" ],
-                    "name": "test_policy"
-                }
-        """
-        pass
-
 
 class IdentitySessionCallback(EventCallback):
     """
